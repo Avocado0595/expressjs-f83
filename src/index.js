6de5adca4a019905a385c3063218a4f8e8e992df
Tran Thanh Xuan <ttxuan0595@gmail.com>
2021-02-18 21:24:39 +0700

    Add: view engine

---
+++ src/index.js
@@ -0,0 +1,27 @@
+const path = require('path');
+const express = require('express');
+const morgan = require('morgan');
+const exphbs  = require('express-handlebars');
+const app = express();
+const port = 8080;
+
+//view engine
+app.engine('.hbs', exphbs({extname:'.hbs'}));
+app.set('view engine', '.hbs');
+app.set('views', path.join(__dirname, "resources/views"));
+
+//morgan
+app.use(morgan('combined'));
+
+//route
+app.get('/', (req, res) => {
+  res.render('home');
+})
+app.get('/about', (req, res) => {
+  res.render('about');
+})
+
+//open port 8080
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+})
